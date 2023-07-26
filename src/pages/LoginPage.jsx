36e26b97182255eed146fd358df21d97a9a4a75d
vgrashal <vgrashal@gmail.com>
2023-07-26 09:47:12 +0530

    Login and Register further improved

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import { Toaster, toast } from 'react-hot-toast';
 
 function LoginPage() {
+
+    const[username, setUsername] = useState('');
+    const[password, setPassword] = useState('');
+
+    const url = "http://localhost:8000/login";
+
+    const toastStyle = {style: {
+      border: '0',
+      borderBottom: '2px solid #e89404',
+      borderRadius: '16px',
+      color: '#000000',
+    },
+    iconTheme: {
+      primary: '#000000',
+      secondary: '#FFFFFF',
+    },
+    duration: 2000,}
+
+    const handleSubmit = () => {
+      axios.post(url, {username, password})
+      .then((res) => {
+        if(res.data === "True"){
+          toast.success("Login success, logging you in...", toastStyle);
+        }
+      })
+    }
+
   return (
+    <>
+    <Toaster 
+    position="top-center"
+    />
     <form className='login'>
         <h1>Login</h1>
-        <input type='text' placeholder='username'/>
-        <input type='password' placeholder='password'/>
-        <button type='submit'>Login</button>
+        <input type='text' value={username} placeholder='username' onChange={(e)=>{setUsername(e.target.value)}}/>
+        <input type='password' value={password} placeholder='password' onChange={(e)=>{setPassword(e.target.value)}}/>
+        <button type='button' onClick={handleSubmit}>Login</button>
     </form>
+    </>
   )
 }
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Toaster, toast } from 'react-hot-toast';
 function RegisterPage() {
+
+  const[username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+
+  const url = "http://localhost:8000/register";
+
+  const toastStyle = {style: {
+    border: '0',
+    borderBottom: '2px solid #e89404',
+    borderRadius: '16px',
+    color: '#000000',
+  },
+  iconTheme: {
+    primary: '#000000',
+    secondary: '#FFFFFF',
+  },
+  duration: 2000,}
+
+  const handleSubmit = () => {
+    axios.post(url, { username, password })
+      .then((res) => {
+        if (res.data === "True") { 
+          toast.error("Username already taken, please choose another username", toastStyle);
+        }
+      })
+      .catch((error) => {
+        console.error("Error creating user:", error);
+      });
+  };
+  
   return (
+    <>
+    <Toaster 
+    position="top-center"
+    />
     <form className='register'>
       <h1>Register</h1>
-        <input type='text' placeholder='username'/>
-        <input type='password' placeholder='password'/>
-        <button type='submit'>Register</button>
+      <input type='text' value = {username} placeholder='username' onChange={(e)=>{setUsername(e.target.value)}}/>
+        <input type='password' value = {password} placeholder='password' onChange={(e)=>{setPassword(e.target.value)}}/>
+        <button type='button' onClick={handleSubmit}>Register</button>
     </form>
+    </>
   )
 }
 
